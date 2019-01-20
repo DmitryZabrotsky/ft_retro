0bb21310819d7d0eb8f888567551625a2283e059
Anastasiia ALOKHINA <aalokhin@e2r4p3.unit.ua>
2019-01-20 12:20:24 +0200

    view

--- View.cpp
+++ View.cpp
@@ -6,16 +6,73 @@
 
 View::View() {
 
+	initscr(); 
+	cbreak(); 
+	noecho(); 
+	curs_set(0);
+
+	keypad(stdscr, TRUE); 
+	nodelay(stdscr, TRUE); 
+	scrollok(stdscr, FALSE);
+
+
+	getmaxyx(stdscr, this->y_wmax, this->x_wmax);
+
+	
+	init_pair(1, COLOR_GREEN, COLOR_BLACK);
+	init_pair(2, COLOR_BLUE, COLOR_BLACK);
+	init_pair(3, COLOR_RED, COLOR_BLACK);
+	init_pair(4, COLOR_CYAN, COLOR_BLACK);
+	init_pair(5, COLOR_WHITE, COLOR_BLACK);
+	init_pair(6, COLOR_RED, COLOR_RED);
+	init_pair(7, COLOR_CYAN, COLOR_BLACK);
+	init_pair(11, COLOR_BLACK, COLOR_GREEN);
+	init_pair(12, COLOR_CYAN, COLOR_CYAN);
+	init_pair(22, COLOR_BLACK, COLOR_BLUE);
+	init_pair(33, COLOR_BLACK, COLOR_RED);
+	init_pair(44, COLOR_BLACK, COLOR_CYAN);
+	init_pair(55, COLOR_BLACK, COLOR_WHITE);
+	getmaxyx(stdscr, y, x);
+
+
+	WINDOW *win = newwin(this->y_wmax - 1, this->x_wmax - 1, 0, 0);
+	nodelay(stdscr, FALSE);
+	init_pair(133, COLOR_RED, COLOR_BLUE);	
+	wattron(win, COLOR_PAIR(133));
+	wborder(win, 42, 42, 42, 42, 42, 42, 42, 42);
+	wrefresh(win);
 }
 
-View::View(View const &obj) {
+View::View(View const &obj) { *this = obj; }
 
-}
-
-View::~View() {
-
-}
+View::~View() { endwin(); }
 
 View &View::operator=(View const &obj) {
-	return <#initializer#>;
+	_win = obj._win;
+	x_wmax = obj.x_wmax;
+	y_wmax = obj.y_wmax;
+	return (*this);
+} 
+
+
+View::printmap(Map const &map)
+{
+	int x1 = 0;
+	int y1 = 0;
+	while (y1 < y_max)
+	{
+		x1 = 0;
+		while(x1 < x_wmax)
+		{
+			wattron(win, map.getColor(x1, y1));
+			mvwprintw(_win, int y1, int x1, %c, getSymbol(x1, y1));
+			x1++;
+		}
+		y1++;
+	}
+	wrefresh(_win);
+
 }
+
+int View::get_x_wmax() {return (this->x_wmax)}
+int View::get_y_wmax() {return (this->y_wmax)}
